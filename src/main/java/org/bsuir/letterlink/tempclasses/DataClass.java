f2a6fc7ac9d3be0741e8f9f82ee47f71ca87a768
freezton <vlad.freezzy@gmail.com>
2023-05-22 19:01:43 +0300

    fix

--- src/main/java/org/bsuir/letterlink/tempclasses/DataClass.java
+++ src/main/java/org/bsuir/letterlink/tempclasses/DataClass.java
@@ -24,12 +24,12 @@ public class DataClass {
     public   final static String smtpPort="25"; //587
     public  final static String imapPort="993";
 
+
 //    public final static String smtpHost="smtp.gmail.com";
 //    public  final static String imapHost="imap.mail.ru";
 //    public  final static String address="qwjvwjl@gmail.com";
 //    public   final static String login="qwjvwjl";
 //    public   final static String to="vlad.freezzy@gmail.com";
-//    public   final static String password="Killer2004top1";
 //    public   final static String smtpPort="587"; //587
 //    public  final static String imapPort="993";
 }
